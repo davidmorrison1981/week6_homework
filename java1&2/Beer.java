cb5667d872bc459e5e70a4e381e853fd82c2d99b
davidmorrison1981 <david.morrison@geminises.com>
2016-08-24 15:36:29 +0100

    restructured folder

--- java1&2/Beer.java
+++ java1&2/Beer.java
@@ -1,5 +1,5 @@
 public class Beer implements Consumable{
   public String robbieLovesCodeClan(){
-    return "Have I told you guys I LOVE CODECLAN";
+    return "Have I told you guys I LOVE CODECLAN spew";
   }  
 }
